216a7ffb4e744ba247b2324df1797da854b913da
marko <artofmark87@gmail.com>
2025-12-24 16:55:37 +0100

    Added inventory.py and InventoryLoader class and implementation.

---
+++ network/inventory.py
@@ -0,0 +1,23 @@
+import yaml
+from pathlib import Path
+
+class InventoryLoader:
+    """
+    Class to load and parse network device inventory from a YAML file.
+    """
+    def __init__(self, inventory_path: str):
+        self.inventory_path: Path = Path(inventory_path)
+
+    def load_inventory(self):
+        # Check if the inventory file exists
+        if not self.inventory_path.exists():
+            raise FileNotFoundError(f"Inventory file not found: {self.inventory_path}")
+        
+        # Load and parse the YAML inventory file
+        with open(self.inventory_path, "r") as file:
+            try:
+                inventory_data: dict[str:str] = yaml.safe_load(file)
+            except yaml.YAMLError as e:
+                raise ValueError(f"Error parsing YAML file: {e}")
+        
+        return inventory_data.get("devices" , [])
